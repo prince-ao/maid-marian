772ab3cb6160ea407883e880da858e343dac9fef
prince-ao <princead@tutanota.com>
2021-11-13 07:29:47 -0500

    Home Logic Finished

--- App.js
+++ App.js
@@ -6,61 +6,65 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Home, Globe, Search, Settings } from "./Tabs";
 import { StockView } from "./Rooms";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
 const BottomTab = () => {
-  <Tab.Navigator
-    initialRouteName="home"
-    screenOptions={({ route }) => ({
-      tabBarShowLabel: false,
-      headerShown: false,
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
-        if (route.name === "home") {
-          iconName = focused ? "home" : "home-outline";
-        } else if (route.name === "search") {
-          iconName = focused ? "search" : "search-outline";
-        } else if (route.name === "globe") {
-          iconName = focused ? "globe" : "globe-outline";
-        } else if (route.name === "settings") {
-          iconName = focused ? "ios-cog" : "ios-cog-outline";
-        }
-        return <Ionicons name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: "#0367fc",
-      tabBarInactiveTintColor: "gray",
-      tabBarStyle: {
-        backgroundColor: "#1a1a1a",
-        borderTopWidth: 0,
-      },
-    })}
-  >
-    <Tab.Screen name="home">{(props) => <Home {...props} />}</Tab.Screen>
-    <Tab.Screen name="search">{(props) => <Search {...props} />}</Tab.Screen>
-    <Tab.Screen name="globe">{(props) => <Globe {...props} />}</Tab.Screen>
-    <Tab.Screen name="settings">
-      {(props) => <Settings {...props} />}
-    </Tab.Screen>
-  </Tab.Navigator>;
+  return (
+    <Tab.Navigator
+      initialRouteName="home"
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "search") {
+            iconName = focused ? "search" : "search-outline";
+          } else if (route.name === "globe") {
+            iconName = focused ? "globe" : "globe-outline";
+          } else if (route.name === "settings") {
+            iconName = focused ? "ios-cog" : "ios-cog-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#0367fc",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          backgroundColor: "#1a1a1a",
+          borderTopWidth: 0,
+        },
+      })}
+    >
+      <Tab.Screen name="home">{(props) => <Home {...props} />}</Tab.Screen>
+      <Tab.Screen name="search">{(props) => <Search {...props} />}</Tab.Screen>
+      <Tab.Screen name="globe">{(props) => <Globe {...props} />}</Tab.Screen>
+      <Tab.Screen name="settings">
+        {(props) => <Settings {...props} />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
 };
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="BottomTab">
-            {(props) => <BottomTab {...props} />}
-          </Stack.Screen>
-          <Stack.Screen name="StockView">
-            {(props) => <StockView {...props} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="BottomTab"
+        screenOptions={{ tabBarShowLabel: false, headerShown: false }}
+      >
+        <Stack.Screen name="BottomTab">
+          {(props) => <BottomTab {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="StockView">
+          {(props) => <StockView {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- Tabs/Home.jsx
+++ Tabs/Home.jsx
@@ -1,11 +1,225 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const [usMarkets, setUsMarkets] = useState([]);
+  const [europeMarkets, setEuropeMarkets] = useState([]);
+  const [asiaMarkets, setAsiaMarkets] = useState([]);
+  const [currencyMarkets, setCurrencyMarkets] = useState([]);
+  const [cryptoMarkets, setCryptoMarkets] = useState([]);
+  const [ratesMarkets, setRatesMarkets] = useState([]);
+  const [futuresMarkets, setFuturesMarkets] = useState([]);
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://74.68.72.242:4000/home/us-markets")
+        .then((res) => res.json())
+        .then((json) => {
+          const json_ = Object.entries(json);
+          //console.log(json_);
+          setUsMarkets(json_);
+        });
+
+      fetch("http://74.68.72.242:4000/home/europe-markets")
+        .then((res) => res.json())
+        .then((json) => {
+          const json_ = Object.entries(json);
+          //console.log(json_);
+          setEuropeMarkets(json_);
+        });
+
+      fetch("http://74.68.72.242:4000/home/asia-markets")
+        .then((res) => res.json())
+        .then((json) => {
+          const json_ = Object.entries(json);
+          //console.log(json_);
+          setAsiaMarkets(json_);
+        });
+
+      fetch("http://74.68.72.242:4000/home/currency-markets")
+        .then((res) => res.json())
+        .then((json) => {
+          const json_ = Object.entries(json);
+          //console.log(json_);
+          setCurrencyMarkets(json_);
+        });
+
+      fetch("http://74.68.72.242:4000/home/crypto-markets")
+        .then((res) => res.json())
+        .then((json) => {
+          const json_ = Object.entries(json);
+          //console.log(json_);
+          setCryptoMarkets(json_);
+        });
+
+      fetch("http://74.68.72.242:4000/home/rates-markets")
+        .then((res) => res.json())
+        .then((json) => {
+          const json_ = Object.entries(json);
+          //console.log(json_);
+          setRatesMarkets(json_);
+        });
+
+      fetch("http://74.68.72.242:4000/home/futures-markets")
+        .then((res) => res.json())
+        .then((json) => {
+          const json_ = Object.entries(json);
+          //console.log(json_);
+          setFuturesMarkets(json_);
+        });
+
+      setTime(`${Math.random() * 1000000}`);
+    }, 1000 * 40);
+    return;
+  }, [time]);
+  // fetch("http://74.68.72.242:4000/home/us-markets")
+  //   .then((res) => res.json())
+  //   .then((json) => {
+  //     const json_ = Object.entries(json);
+  //     //console.log(json_);
+  //     setUsMarkets(json_);
+  //   });
   return (
-    <View>
-      <Text>Hello Home</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>US Markets</Text>
+          <ScrollView horizontal={true}>
+            {usMarkets.map((usMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{usMarket[1][0]}</Text>
+                  <Text>{usMarket[1][1]}</Text>
+                  <Text>{usMarket[1][2]}</Text>
+                  <Text>{usMarket[1][3]}</Text>
+                  <Text>{usMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <View>
+          <Text>Europe Markets</Text>
+          <ScrollView horizontal={true}>
+            {europeMarkets.map((europeMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{europeMarket[1][0]}</Text>
+                  <Text>{europeMarket[1][1]}</Text>
+                  <Text>{europeMarket[1][2]}</Text>
+                  <Text>{europeMarket[1][3]}</Text>
+                  <Text>{europeMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <View>
+          <Text>Asia Markets</Text>
+          <ScrollView horizontal={true}>
+            {asiaMarkets.map((asiaMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{asiaMarket[1][0]}</Text>
+                  <Text>{asiaMarket[1][1]}</Text>
+                  <Text>{asiaMarket[1][2]}</Text>
+                  <Text>{asiaMarket[1][3]}</Text>
+                  <Text>{asiaMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <View>
+          <Text>Forex Markets</Text>
+          <ScrollView horizontal={true}>
+            {currencyMarkets.map((currencyMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{currencyMarket[1][0]}</Text>
+                  <Text>{currencyMarket[1][1]}</Text>
+                  <Text>{currencyMarket[1][2]}</Text>
+                  <Text>{currencyMarket[1][3]}</Text>
+                  <Text>{currencyMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <View>
+          <Text>Crypto Markets</Text>
+          <ScrollView horizontal={true}>
+            {cryptoMarkets.map((cryptoMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{cryptoMarket[1][0]}</Text>
+                  <Text>{cryptoMarket[1][1]}</Text>
+                  <Text>{cryptoMarket[1][2]}</Text>
+                  <Text>{cryptoMarket[1][3]}</Text>
+                  <Text>{cryptoMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <View>
+          <Text>Rates Markets</Text>
+          <ScrollView horizontal={true}>
+            {ratesMarkets.map((ratesMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{ratesMarket[1][0]}</Text>
+                  <Text>{ratesMarket[1][1]}</Text>
+                  <Text>{ratesMarket[1][2]}</Text>
+                  <Text>{ratesMarket[1][3]}</Text>
+                  <Text>{ratesMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <View>
+          <Text>Futures Markets</Text>
+          <ScrollView horizontal={true}>
+            {futuresMarkets.map((futuresMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{futuresMarket[1][0]}</Text>
+                  <Text>{futuresMarket[1][1]}</Text>
+                  <Text>{futuresMarket[1][2]}</Text>
+                  <Text>{futuresMarket[1][3]}</Text>
+                  <Text>{futuresMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+
+        <View>
+          <Text>Futures Markets</Text>
+          <ScrollView horizontal={true}>
+            {futuresMarkets.map((futuresMarket, key) => {
+              return (
+                <View key={key}>
+                  <Text>{futuresMarket[1][0]}</Text>
+                  <Text>{futuresMarket[1][1]}</Text>
+                  <Text>{futuresMarket[1][2]}</Text>
+                  <Text>{futuresMarket[1][3]}</Text>
+                  <Text>{futuresMarket[1][4]}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
